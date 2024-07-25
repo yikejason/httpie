d729f665a93e9d04da9ef0ddd560f72c737e43e5
yikejason <18215513386@163.com>
2024-07-25 10:06:22 +0800

    feature: Add functions parse_url_works and parse_kv_pair_works for testing.

---
+++ README.md
@@ -0,0 +1,8 @@
+### Crate
+- clap crate
+
+### install
+- First, clone this repository to your local computer.
+- Then, run `cargo install --path .` to install it into your environment.
+- After installation, use the httpie command to search for available commands.
+- Currently, this project only supports two commands: get and post.
--- src/opts.rs
+++ src/opts.rs
@@ -67,3 +67,34 @@ impl FromStr for KvPair {
 fn parse_kv_pair(s: &str) -> Result<KvPair> {
     s.parse()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_url_works() {
+        assert!(parse_url("abc").is_err());
+        assert!(parse_url("http://abc.xyz").is_ok());
+        assert!(parse_url("https://httpbin.org/post").is_ok());
+    }
+
+    #[test]
+    fn parse_kv_pair_works() {
+        assert!(parse_kv_pair("a").is_err());
+        assert_eq!(
+            parse_kv_pair("a=1").unwrap(),
+            KvPair {
+                k: "a".into(),
+                v: "1".into()
+            }
+        );
+        assert_eq!(
+            parse_kv_pair("a=").unwrap(),
+            KvPair {
+                k: "a".into(),
+                v: "".into()
+            }
+        );
+    }
+}
